6a27d4b9d7429192d442c2f9473ffa9cb1c8bf0d
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-20 04:49:29 +0300

    Create UserInterest model.

--- spec/models/user_interest_spec.rb
+++ spec/models/user_interest_spec.rb
@@ -14,7 +14,7 @@
 #  index_user_interests_on_profile_id_and_tag_id  (profile_id,tag_id) UNIQUE
 #  index_user_interests_on_tag_id                 (tag_id)
 #
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe UserInterest, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
