456355ff8863cebb0339c52175266fa0139f6da8
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-21 15:28:07 +0300

    Delete useless migrations.

--- db/migrate/20220120121938_add_unique_index_to_user_interests.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Migration add unique index to user_interests table.
-class AddUniqueIndexToUserInterests < ActiveRecord::Migration[6.1]
-  def change
-    add_index :user_interests, %i[profile_id tag_id], unique: true
-  end
-end
