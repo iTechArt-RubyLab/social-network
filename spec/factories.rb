b76003fe26065e2d7bba1c179782146a7a6859a6
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-21 17:59:39 +0300

    Add post_tag spec.

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,9 @@ FactoryBot.define do
   factory :tag do
     name { Faker::Hobby.unique.activity }
   end
+
+  factory :post_tag do
+    tag_id { Tag.all.sample.id }
+    post_id { Post.all.sample.id }
+  end
 end
