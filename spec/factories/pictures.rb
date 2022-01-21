125421e63f469970e8995d2cd72fe510e96fd45b
skripchenkoveta <skripchenkoVeta@gmail.com>
2022-01-21 03:19:06 +0300

    SN-30|SN-31/add tests for model

--- spec/factories/pictures.rb
+++ spec/factories/pictures.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
-  factory :picture do
-   
-  end
+  factory :picture,class: Picture do
+    
+    trait :invalid do
+   picture { Rack::Test::UploadedFile.new("#{::Rails.root}/1.jpg") }
+    end
 
+    trait :empty do
+      picture { nil }
+    end
+  end
 end
--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Picture, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:invalid) { FactoryBot.build :picture, :invalid }
+  let(:empty) { FactoryBot.build :picture, :empty }
+
+  context 'with valid attributes' do
+    it { expect(invalid).to_not be_valid }
+  end
+  context 'with empty attributes' do
+    it { expect(empty).to_not be_valid }
+  end
 end
