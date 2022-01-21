3ff8998adad7e16c4157319ecdbe8fe071924ab6
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-21 11:39:06 +0300

    Remove redunant faker requirment in seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require "faker"
 require "factory_bot_rails"
 
 GENERATE_TAGS_COUNT = 30
