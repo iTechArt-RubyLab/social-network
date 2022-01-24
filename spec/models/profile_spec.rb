4d43957afb63710e93aa42d8c2a34eb6eb50ea78
Slavahaim <slava.irizanov@gmail.com>
2022-01-24 08:08:18 +0300

    Fixed rubo-style

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -23,47 +23,48 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  subject(:profile) {FactoryBot.build(:profile)}
-  describe 'validation tests'do
-    context 'when we have required fields to fill in ' do
+  subject(:profile) { FactoryBot.build(:profile) }
 
+  describe 'validation tests' do
+    context 'when we have required fields to fill in ' do
       it 'ensures surname presecence' do
-        profile.surname = nil 
-        expect(profile.save).to eq(false)  
+        profile.surname = nil
+        expect(profile.save).to eq(false)
       end
       it 'ensures name presecence' do
-        profile.name = nil 
-        expect(profile.save).to eq(false)  
+        profile.name = nil
+        expect(profile.save).to eq(false)
       end
       it 'ensures birthday presecence' do
-        profile.birthday = nil 
-        expect(profile.save).to eq(false)  
+        profile.birthday = nil
+        expect(profile.save).to eq(false)
       end
       it 'ensures email presecence' do
-        profile.email = nil 
-        expect(profile.save).to eq(false)  
+        profile.email = nil
+        expect(profile.save).to eq(false)
       end
       it 'ensures phone presecence' do
-        profile.phone = nil 
-        expect(profile.save).to eq(false)  
+        profile.phone = nil
+        expect(profile.save).to eq(false)
       end
       it 'ensures about presecence' do
-        profile.about = nil 
-        expect(profile.save).to eq(false)  
+        profile.about = nil
+        expect(profile.save).to eq(false)
       end
-      it 'should save successfully' do
-        expect(profile.save).to eq(true)  
+      it 'saves successfully' do
+        expect(profile.save).to eq(true)
       end
     end
   end
+
   describe 'phone limit test' do
     context 'when phone limit not more than 15 digits' do
       it 'ensures phone limit <= 15 ' do
-        expect(profile.phone.size <= 15).to eq(true) 
+        expect(profile.phone.size <= 15).to eq(true)
       end
       it 'return error ' do
-        profile.phone = '1234567890123456' 
-        expect(profile.save).to eq(false) 
+        profile.phone = '1234567890123456'
+        expect(profile.save).to eq(false)
       end
     end
   end
