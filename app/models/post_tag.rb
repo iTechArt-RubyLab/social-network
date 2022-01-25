bffdabd0b987e8a5b7b479d8b69eb2c517500ddf
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-25 13:18:01 +0300

    Annotate models.

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: post_tags
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :bigint
+#  tag_id     :bigint
+#
+# Indexes
+#
+#  index_post_tags_on_post_id             (post_id)
+#  index_post_tags_on_post_id_and_tag_id  (post_id,tag_id) UNIQUE
+#  index_post_tags_on_tag_id              (tag_id)
+#
 class PostTag < ApplicationRecord
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  net_state  :datetime         not null
+#  status     :integer          default("active"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  profile_id :integer          not null
+#
+# Indexes
+#
+#  index_users_on_profile_id  (profile_id) UNIQUE
+#
 FactoryBot.define do
   factory :user do
     status { 'active' }
