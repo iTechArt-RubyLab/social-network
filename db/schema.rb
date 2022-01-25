7b70293d846b4a15858359d3719f7e428e7efb76
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-25 13:11:28 +0300

    Create Tag model.

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 2022_01_20_122409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+  
   create_table "users", force: :cascade do |t|
     t.integer "status", default: 0, null: false
     t.datetime "net_state", null: false
