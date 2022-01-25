a5c6e632d612dc57503716e4b9600dc7ba297c79
Elizaveta <70839826+skripchenkoVeta@users.noreply.github.com>
2022-01-25 14:27:06 +0300

    SN-26/Create Like model (#12)
    * SN-26/create like model
    
    * SN-26/add factory for like
    
    * SN-26/add spec for like
    
    * SN-26/fix style
    
    * SN-26/add gem shoulda-matchers
    
    * SN-26/add schema.rb
    
    * SN-26/fix spec
    
    * SN-26/rename polymorphic association

---
+++ app/models/like.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: likes
+#
+#  id            :bigint           not null, primary key
+#  likeable_type :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  likeable_id   :bigint
+#  user_id       :bigint
+#
+# Indexes
+#
+#  index_likes_on_likeable  (likeable_type,likeable_id)
+#  index_likes_on_user_id   (user_id)
+#
+class Like < ApplicationRecord
+  belongs_to :likeable, polymorphic: true
+  belongs_to :user
+end
