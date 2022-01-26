83e3dd7640b35eb1f57700c7c2c796699ae14d6c
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-26 21:36:28 +0300

    Add description of subcriptions create migration class.

--- db/migrate/20220126163422_create_subscriptions.rb
+++ db/migrate/20220126163422_create_subscriptions.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Migration create subscriptions table in database.
 class CreateSubscriptions < ActiveRecord::Migration[6.1]
   def change
     create_table :subscriptions do |t|
