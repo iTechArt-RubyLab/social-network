ac143c2238e20f4fc58dcc5046033c998998ff25
mlshvch <malashevichdev@gmail.com>
2022-01-26 17:07:52 +0300

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
-FactoryBot.create_list(:user, 6)
-FactoryBot.create_list(:message, 10)
+USERS_WITH_MULTIPLE_MESSAGES = 2
+USERS_WITH_NO_MESSAGES = 3
+USERS_WITH_A_SINGLE_MESSAGE = 5
+NUMBER_OF_USERS = USERS_WITH_MULTIPLE_MESSAGES + USERS_WITH_NO_MESSAGES + USERS_WITH_A_SINGLE_MESSAGE
+
+MIN_LIMIT_OF_MESSAGES = 3
+MAX_LIMIT_OF_MESSAGES = 7
+
+users = FactoryBot.create_list(:user, NUMBER_OF_USERS)
+
+USERS_WITH_MULTIPLE_MESSAGES.times do
+  user = users.delete(users.sample)
+  FactoryBot.create_list(:message, rand(MIN_LIMIT_OF_MESSAGES..MAX_LIMIT_OF_MESSAGES), user: user)
+end
+
+USERS_WITH_A_SINGLE_MESSAGE.times do
+  user = users.delete(users.sample)
+  FactoryBot.create(:message, user: user)
+end
