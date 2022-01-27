2e6d781b71a80d8ce999fa99220569aa889263fd
Andrew <72628255+PivovarAndrew@users.noreply.github.com>
2022-01-27 14:45:58 +0300

    Change the spelling of adding foreign keys to subscriptions table to more correct.

--- db/migrate/20220126200624_create_subscriptions.rb
+++ db/migrate/20220126200624_create_subscriptions.rb
@@ -4,15 +4,13 @@
 class CreateSubscriptions < ActiveRecord::Migration[6.1]
   def change
     create_table :subscriptions do |t|
-      t.references :subscriber, null: false
-      t.references :signatory, null: false
+      t.references :subscriber, null: false, foreign_key: { to_table: :users }
+      t.references :signatory, null: false, foreign_key: { to_table: :users }
       t.boolean :status, null: false, default: false
 
       t.timestamps
     end
 
-    add_foreign_key :subscriptions, :users, column: :subscriber_id
-    add_foreign_key :subscriptions, :users, column: :signatory_id
     add_index :subscriptions, %i[subscriber_id signatory_id], unique: true
   end
 end
