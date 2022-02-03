ea4adad7223aa7de2727f6b728bf12da02241855
mlshvch <malashevichdev@gmail.com>
2022-02-03 12:37:21 +0300

    Update Application Policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# ApplicationPolicy
 class ApplicationPolicy
   attr_reader :user, :record
 
@@ -36,6 +37,7 @@ class ApplicationPolicy
     false
   end
 
+  # ApplicationPolicy Scope
   class Scope
     def initialize(user, scope)
       @user = user
@@ -49,5 +51,11 @@ class ApplicationPolicy
     private
 
     attr_reader :user, :scope
+  end
+
+  private
+
+  def owns_record?
+    record.user == user
   end
 end
