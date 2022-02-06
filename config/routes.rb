46cc8a1c8cf0a31a842fc88ef224588926b823f8
mlshvch <malashevichdev@gmail.com>
2022-02-06 17:24:48 +0300

    Add Post endpoints to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :profiles, only: %i[index show update create]
+      resources :posts
     end
   end
 end
