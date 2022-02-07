c7af3d67aed48b67bf7ba5ad668e37574e11349a
mlshvch <malashevichdev@gmail.com>
2022-02-07 22:18:27 +0300

    Update Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit
+  before_action :authenticate_user!
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  private
+
+  def not_authorized
+    render json: '403 Forbidden', status: :forbidden
+  end
 end
