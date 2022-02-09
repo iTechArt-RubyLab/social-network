3fcbbb2178f14da26a375d72a04707960ae5c8fe
Aliaksei Malashevich <87980319+mlshvch@users.noreply.github.com>
2022-02-09 10:38:30 +0300

    Fix authentication (#35)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
 
   private
 
   def not_authorized
-    render json: '403 Forbidden', status: :forbidden
+    if user_signed_in?
+      render json: '403 Forbidden', status: :forbidden
+    else
+      render json: '401 Unauthorized', status: :unauthorized
+    end
   end
 end
